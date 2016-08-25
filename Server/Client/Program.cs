c1f077f7794193b70339e5d4dcdde131d85b5a33
PanicJoker03 <buddynod32@hotmail.com>
2016-08-25 11:54:52 -0500

    Cliente y servidor encapsulado de manera apropiada, ahora solo tengo que trabajar en Forms

--- Server/Client/Program.cs
+++ Server/Client/Program.cs
@@ -9,40 +9,19 @@ using EasyPOI;
 namespace Client
 {
     class Program {
-        //private static Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private const int Port = 100;
         private static EasyPOI.Client cliente;
-        ////https://msdn.microsoft.com/en-us/library/bew39x2a(v=vs.110).aspx
-        //public class StateObject
-        //{
-        //    // Client socket.
-        //    public Socket workSocket = null;
-        //    // Size of receive buffer.
-        //    public const int BufferSize = 256;
-        //    // Receive buffer.
-        //    public byte[] buffer = new byte[BufferSize];
-        //    // Received data string.
-        //    public StringBuilder sb = new StringBuilder();
-        //}
         static void Recibir(Packet packet)
         {
-            Console.WriteLine("Recibiste un paquete de tipo: {0}", packet.PacketType);
+            Console.WriteLine("Recibiste un paquete de tipo: {0}", packet.Type);
         }
         static void Main(string[] args) {
             cliente = new EasyPOI.Client();
             cliente.OnConnection = () => { Console.WriteLine("conectado"); };//Conectado;
             cliente.OnConnectionFail = () => { Console.WriteLine("conexion fallida"); };//ConexionFallida;
+            cliente.OnPacketReceived = Recibir;
             cliente.BeginConnect();
-            //LoopConnect();
             SendLoop();
         }
-
-        //private static void Exit() {
-        //    SendString("exit");
-        //    client.Shutdown(SocketShutdown.Both);
-        //    client.Close();
-        //    Environment.Exit(0);
-        //}
 
         private static void SendLoop() {
             while(true)
@@ -59,40 +38,16 @@ namespace Client
                     {
                         SendString(msg);
                     }
-                    //string received = ReceiveString();
-                    //Console.WriteLine(received);
                 }
             }
         }
-
-        //private static void LoopConnect() {
-        //    int attempts = 0;
-        //    //while(!client.Connected) {
-        //    //    try {
-        //    //        attempts++;
-        //    //        client.Connect(IPAddress.Loopback, Port);
-        //    //    } catch (SocketException) {
-        //    //        Console.Clear();
-        //    //        Console.WriteLine("Connection attempts: {0}", attempts);
-        //    //    }
-        //    //}
-        //}
-
-        //private static string ReceiveString()
-        //{
-        //    byte[] receivedBuffer = new byte[1024];
-        //    int rec = client.Receive(receivedBuffer);
-        //    byte[] data = new byte[rec];
-        //    Buffer.BlockCopy(receivedBuffer, 0, data, 0, rec);
-        //    return Encoding.ASCII.GetString(data);
-        //}
 
         private static void SendString(string text)
         {
             byte[] buffer = Encoding.ASCII.GetBytes(text);
             if (buffer.Length > 0)
             {
-                Packet packet = new Packet("message", buffer);
+                Packet packet = new Packet(PacketType.Text, text);
                 cliente.SendPacket(packet);
             }
         }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -5,70 +5,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using EasyPOI;
 namespace Server {
     class Program {
-        private static Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        private static List<Socket> clients = new List<Socket>();
-        private static byte[] dataBuffer = new byte[1024];
-        private const int Port = 100;
-
+        private static EasyPOI.Server server;
+        private static void Received(Packet packet)
+        {
+            Console.Write("Paquete recibido: ");
+            if(packet.Type == PacketType.Text)
+            {
+                Console.WriteLine(packet.GetContent<string>());
+            }
+        }
         static void Main(string[] args) {
-            SetUp();
+            server = new EasyPOI.Server();
+            server.OnClientAccepted = () => { Console.WriteLine("Cliente conectado"); };
+            server.OnPacketReceived = Received;
+            Console.WriteLine("Buscando clientes");
+            server.StartListening();
             Console.ReadLine();
         }
-
-        private static void SetUp() {
-            Console.WriteLine("Setting up server...");
-            server.Bind(new IPEndPoint(IPAddress.Any, Port));
-            server.Listen(0);
-            server.BeginAccept(new AsyncCallback(AcceptCallback), null);
-     
-        }
-
-        private static void AcceptCallback(IAsyncResult AR) {
-            Socket client = server.EndAccept(AR);
-            Console.WriteLine("Client connected");
-            clients.Add(client);
-            client.BeginReceive(dataBuffer, 0, dataBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
-            server.BeginAccept(new AsyncCallback(AcceptCallback), null);
-
-        }
-
-        private static void ReceiveCallback(IAsyncResult AR) {
-            Socket client = (Socket)AR.AsyncState;
-            int received;
-            try {
-                received = client.EndReceive(AR);
-            } catch (SocketException) {
-                Console.WriteLine("Client forcefully disconnected");
-                client.Close();
-                clients.Remove(client);
-                return;
-            }
-            byte[] data = new byte[received];
-            Buffer.BlockCopy(dataBuffer, 0, data, 0, received);
-            string text = Encoding.ASCII.GetString(data);
-
-            string response = string.Empty;
-           if (text.ToLower() == "exit") {
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-                clients.Remove(client);
-                Console.WriteLine("Client disconnected");
-                return;
-            } else {
-                response = text;
-            }
-            byte[] responseData = Encoding.ASCII.GetBytes(response);
-            foreach (Socket chatMember in clients) {
-                chatMember.BeginSend(responseData, 0, responseData.Length, SocketFlags.None, new AsyncCallback(SendCallback), chatMember);
-                chatMember.BeginReceive(dataBuffer, 0, dataBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), chatMember);
-            }
-        }
-
-        private static void SendCallback(IAsyncResult AR) {
-            Socket client = (Socket)AR.AsyncState;
-            client.EndSend(AR);
-        } 
     }
 }
