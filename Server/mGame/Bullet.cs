fd08fa05790fc795a639d1e06ad33120ce469857
Luis Núñez <buddynod32@hotmail.com>
2016-10-30 15:01:47 -0600

    Add files via upload

---
+++ Server/mGame/Bullet.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace mGame
+{
+    class Bullet : Updateable
+    {
+        protected GraphicInstance sprite;
+        protected new LevelState state;
+        protected Position position;
+        protected Hitbox hitbox;
+        private Texture2D texture;
+        private Vector2 direction;
+        float speed;
+        float rotation;
+        public Bullet(Texture2D texture, int x, int y, float speed, Direction direction)
+        {
+            position = new Position();
+            position.Value = new Vector2(x, y);
+            this.speed = speed;
+            this.texture = texture;
+            switch(direction)
+            {
+                case Direction.Left:
+                    this.direction = new Vector2(-1.0f, 0.0f);
+                    rotation = 180.0f;
+                    break;
+                case Direction.Right:
+                    this.direction = new Vector2(1.0f, 0.0f);
+                    break;
+                case Direction.Up:
+                    this.direction = new Vector2(0.0f, -1.0f);
+                    rotation = 270.0f;
+                    break;
+                case Direction.Down:
+                    this.direction = new Vector2(0.0f, 1.0f);
+                    rotation = 90.0f;
+                    break;
+            }
+            hitbox = new Hitbox("playerBullet", OnCollide, 24, 24, position, new Vector2(-12, -12));
+        }
+        public override void Added()
+        {
+            state = (LevelState)POIGame.CurrentState;
+            sprite = new GraphicInstance(texture, position);
+            state.AddGraphic(sprite);
+            sprite.rotation = MathHelper.ToRadians(rotation);
+            sprite.origin = new Vector2(12.0f, 12.0f);
+            base.Added();
+            state.AddHitbox(hitbox);
+        }
+        internal override void Update()
+        {
+            position.Value += direction * speed;
+            base.Update();
+            if (Vector2.Distance(position.Value, state.camera.Value.Center.ToVector2()) > (POIGame.GameWidth / 2) + (POIGame.GameWidth / 6))
+            {
+                state.RemoveInstance(this);
+            }
+            if(!CheckEmpty())
+            {
+                state.RemoveInstance(this);
+            }
+        }
+        private bool CheckEmpty()
+        {
+            int index = ((int)(position.Value.Y / 24) * LevelState.LevelHeight) + (int)(position.Value.X / 24);
+            return state.LevelData[index] != 0;
+        }
+        protected void OnCollide(string group1, string group2)
+        {
+            state.RemoveInstance(this);
+        }
+        public override void Removed()
+        {
+            state.AddInstance(
+                new OnceAnimation(
+                    Assets.bulletExplode,
+                    (int)position.Value.X -24,
+                    (int)position.Value.Y -24,
+                    48,
+                    48));
+            state.RemoveHitbox(hitbox);
+            state.RemoveGraphic(sprite);
+            base.Removed();
+        }
+    }
+    //component that remove itself when animation ends
+    public class OnceAnimation : Updateable
+    {
+        private GraphicInstance graphic;
+        private Position position;
+        private Animation animation;
+        private Texture2D texture;
+        private float animationSpeed;
+        public OnceAnimation(Texture2D texture, int x, int y, int frameWidth, int frameHeight, float speed = 45.0f)
+        {
+            animationSpeed = speed;
+            this.texture = texture;
+            position = new Position();
+            position.Value = new Vector2(x, y);
+        }
+        public override void Added()
+        {
+            base.Added();
+            graphic = new GraphicInstance(texture, position);
+            animation = new Animation(graphic, 48, 48);
+            int[] frames = new int[texture.Width / animation.Frame.Width];
+            for(int i = 0; i < frames.Length; i++) { frames[i] = i; }
+            animation.AddAnimation("base", frames);
+            animation.SetAnimation("base", animationSpeed);
+            state.AddGraphic(graphic);
+            state.AddAnimation(animation);
+        }
+        internal override void Update()
+        {
+            base.Update();
+            if (animation.AnimationEnd)
+                state.RemoveInstance(this);
+        }
+        public override void Removed()
+        {
+            state.RemoveGraphic(graphic);
+            state.RemoveAnimation(animation);
+            base.Removed();
+        }
+    }
+}
